969baba66817dd8ac160af760587d0e8ed7d37f5
Polpsi <59441238+Polpsi@users.noreply.github.com>
2021-02-11 22:23:09 +0300

    BeforeLesson3

---
+++ src/main/java/Lesson3/P1_PingPong/main.java
@@ -0,0 +1,7 @@
+package Lesson3.P1_PingPong;
+
+public class main {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/main/java/Lesson3/P2_Counter/main.java
@@ -0,0 +1,7 @@
+package Lesson3.P2_Counter;
+
+public class main {
+    public static void main(String[] args) {
+
+    }
+}
